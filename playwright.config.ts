7072f1caf7c0d7ba15cb8293206dd5e18d754c23
muratozcan-seon <murat.ozcan@seon.io>
2025-03-27 09:21:49 -0500

    test: basic ci

---
+++ playwright.config.ts
@@ -0,0 +1,23 @@
+import { config as dotenvConfig } from 'dotenv'
+import path from 'path'
+
+dotenvConfig({
+  path: path.resolve(__dirname, '../../.env')
+})
+
+const envConfigMap = {
+  dev: require('./playwright/config/dev.config').default
+  // add envs as needed
+}
+
+const environment = process.env.TEST_ENV || 'dev'
+
+// Validate environment config
+if (!Object.keys(envConfigMap).includes(environment)) {
+  console.error(`No configuration found for environment: ${environment}`)
+  console.error('Available environments:')
+  Object.keys(envConfigMap).forEach((env) => console.error(`- ${env}`))
+  process.exit(1)
+}
+
+export default envConfigMap[environment]
